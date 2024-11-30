f4f11edaaaaa09649313d220d6559d7bcd15be77
celeste456 <85599011+celeste456@users.noreply.github.com>
2024-11-30 11:51:01 -0600

    Modulo de citas finalizado

--- CapaDePresentacion/CapaDePresentacion/obj/Debug/net8.0/CapaDePresentacion.AssemblyInfo.cs
+++ CapaDePresentacion/CapaDePresentacion/obj/Debug/net8.0/CapaDePresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaDePresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b207e90042ed051e917b12f77540b5838536122b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaDePresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaDePresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
